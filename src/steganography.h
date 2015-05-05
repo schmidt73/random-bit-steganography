92ef428c82c2a56882c74dfd948b84145335964d
loaner <loaner@D002324.local>
2015-05-04 21:36:42 -0500

    added steganography.h

---
+++ src/steganography.h
@@ -0,0 +1,18 @@
+#ifndef STEGANOGRAPHY_HEADER
+#define STEGANOGRAPHY_HEADER
+
+#include "random.h"
+#include <stdlib.h>
+#include <getopt.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "methods/png.h"
+
+const char* get_filename_ext(const char *filename);
+void print_version();
+void print_usage();
+int init_decrypting_data(const char* type, const char* inputfile, unsigned long int* key);
+int init_encrypting_data(const char* type, const char* inputfile, unsigned long int* key);
+
+#endif
